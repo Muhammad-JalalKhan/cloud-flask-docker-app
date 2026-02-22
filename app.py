06587d08af67c1c1a3eaa1795d271949644eaf03
Jalal Khan <itxjalalkhanofficial@gmail.com>
2026-02-22 23:03:22 +0500

    print Myself name on the site

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_cloud():
     # This returns the required "Hello Cloud" message
-    return "<h1>Hello Cloud</h1><p>Running inside a Docker Container!</p>"
+    return "<h1>Hello Muhammad Jalal</h1><p>Running inside a Docker Container!</p>"
 
 if __name__ == "__main__":
     # Standard Flask port for the assignment
